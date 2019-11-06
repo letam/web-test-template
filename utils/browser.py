79b3b45925bbb4fe243f02085c0f92fa805ef03c
Tam Le <tamsgit@gmail.com>
2019-11-06 16:01:28 -0500

    Add more methods to browser util

--- utils/browser.py
+++ utils/browser.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import (
     InvalidSelectorException,
     NoSuchElementException,
 )
+from selenium.webdriver.support.ui import Select
 
 from settings import DRIVERS, WAIT_TIME, POLL_FREQUENCY
 
@@ -65,21 +66,61 @@ class BrowserFunctionsMixin:
         except InvalidSelectorException:
             return False
 
-    def find_by_text(self, text: str, timeout=0):
+    def find_by_text(self, text: str, timeout=0, tag_name: str = '*'):
+        ''' Note: If tag name is not provided, then may not return the
+            direct element containing text. '''
         return self.wait_for_element_by_xpath(
-            f'//*[contains(string(), "{text}")]', timeout
+            f'//{tag_name}[contains(string(), "{text}")]', timeout
         )
 
     def find_button_by_text(self, text: str, timeout=0):
-        return self.wait_for_element_by_xpath(
-            f'//button[contains(string(), "{text}")]', timeout
-        )
+        return self.find_by_text(text, timeout, 'button')
+
+    def find_a_by_text(self, text: str, timeout=0):
+        return self.find_by_text(text, timeout, 'a')
 
     def find_by_name(self, name: str, timeout=0):
         return self.wait_for(lambda: self.driver.find_element_by_name(name), timeout)
 
     def fill(self, field_name: str, value: str):
         self.find_by_name(field_name).send_keys(value)
+
+    def get_url_domain_name(self):
+        return self.driver.current_url.split('//')[1].split('/', 1)[0]
+
+    def get_url_path(self):
+        return self.driver.current_url.split('//')[1].split('/', 1)[1]
+
+    @property
+    def browser_name(self):
+        return self.driver.capabilities.get('browserName')
+
+    def click(self, element):
+        """ Tell driver to click provided element.
+            If browser is Safari, click via javascript, since
+            `element.click()` does not seem to work on safari
+        """
+        if self.browser_name == 'Safari':
+            self.driver.execute_script('arguments[0].click()', element)
+        else:
+            element.click()
+
+    def select_option_by_visible_text(self, element, option_text: str):
+        select = Select(element)
+        select.select_by_visible_text(option_text)
+
+    def scroll_into_view(self, element):
+        self.driver.execute_script('arguments[0].scrollIntoView()', element)
+
+    def click_checkbox(self, element):
+        ''' Ensure that checkbox element appears so that we can indeeed click it. '''
+        self.driver.execute_script(
+            'arguments[0].style["-moz-appearance"] = "checkbox"', element
+        )
+        self.driver.execute_script(
+            'arguments[0].style["-webkit-appearance"] = "checkbox"', element
+        )
+        self.click(element)
 
 
 def Browser(driver_name: str = "firefox", *args, **kwargs):
